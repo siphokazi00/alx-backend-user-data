bccbf234a93d6ad72617214cd8e43303e933bc6e
siphokazi <usphow@gmail.com>
2024-12-31 17:04:28 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    """Endpoint to test 401 error handler."""
+    abort(401)
